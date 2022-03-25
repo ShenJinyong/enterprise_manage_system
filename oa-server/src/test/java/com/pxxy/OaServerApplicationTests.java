2a1cf191efcaf79086ec013e3bb2f77510f6fee9
ShenJinyong <438217638@qq.com>
2022-03-25 22:41:47 +0800

    【优化】初始化oa后端SpringBoot服务

---
+++ oa-server/src/test/java/com/pxxy/OaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
